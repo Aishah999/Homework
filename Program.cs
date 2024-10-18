2ca14a9041e0e171751a1bd2de2af8ccf1bd860e
Aishah999 <aishaah@code.edu.az>
2024-10-19 00:17:46 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Homework;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //#region
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //int count = 0;
+        //int i = 1;
+
+        //while(i<=9)
+        //{
+        //if(n%i==0)
+        //{
+        //count++;
+        //}
+        //i++;
+        //}
+        //if(count>2)
+        //{
+        //Console.WriteLine("Eded Murekkebdir");
+        //}
+        //else
+        //{
+        //Console.WriteLine("Eded Sadedir");
+        //}
+
+
+
+        //#endregion
+
+        //#region
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //int count = 0;
+        //do
+        //{
+            //count++;
+            //n = n / 10;
+
+        //} while (n != 0);
+        //Console.WriteLine(count);
+
+
+
+        //#endregion
+    }
+}
+
